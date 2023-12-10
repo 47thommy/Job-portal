2328fa2e3cfe099b95b000ed77aeb9bf7567f2ce
47thommy <47thommy@gmail.com>
2023-12-10 11:37:49 +0300

    added login and signup

---
+++ src/main/java/com/jobportal/login/LoginServlet.java
@@ -0,0 +1,51 @@
+package com.jobportal.login;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		Connection con = null;
+		HttpSession session = request.getSession();
+		RequestDispatcher dispatcher = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/job_portal?useSSL=false", "root", "Emebet@1994");
+			PreparedStatement pst = con.prepareStatement("select * from users where email=? and password=?");
+			pst.setString(1, email);
+			pst.setString(2, password);
+			
+			ResultSet rs = pst.executeQuery();
+			
+			if (rs.next()) {
+				session.setAttribute("email", rs.getString("email"));
+				dispatcher=request.getRequestDispatcher("index.jsp");
+			} else {
+				request.setAttribute("status", "failed");
+				dispatcher = request.getRequestDispatcher("login.jsp");
+			}
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();		}
+	
+	}
+
+}
---
+++ src/main/java/com/jobportal/register/RegistrationServlet.java
@@ -0,0 +1,64 @@
+package com.jobportal.register;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Servlet implementation class RegistrationServlet
+ */
+@WebServlet("/register")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    public RegistrationServlet() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("name");
+		String email = request.getParameter("email");
+		String password = request.getParameter("pass");
+		RequestDispatcher dispatcher = null;
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/job_portal?useSSL=false", "root", "Emebet@1994");
+			PreparedStatement pst = con.prepareStatement("insert into users(username, password, email) values(?,?,?)");
+			pst.setString(1, username);
+			pst.setString(2,password);
+			pst.setString(3,email);
+			
+			int rowCount = pst.executeUpdate();
+			
+			dispatcher = request.getRequestDispatcher("registration.jsp");
+			
+			if (rowCount>0) {
+				request.setAttribute("status", "success");
+			} else {
+				request.setAttribute("status", "failed");
+			}
+			dispatcher.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+			
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+}
